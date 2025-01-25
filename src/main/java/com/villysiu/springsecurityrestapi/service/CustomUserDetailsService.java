c23035d282123c4c6ffec6b0ab5d91d3e2c9b777
Villy Siu <villysiu@gmail.com>
2025-01-24 19:27:36 -0800

    extend UserDetailsService

---
+++ src/main/java/com/villysiu/springsecurityrestapi/service/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.villysiu.springsecurityrestapi.service;
+
+import com.villysiu.springsecurityrestapi.model.User;
+import com.villysiu.springsecurityrestapi.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(email + " not found." ));
+
+        Set<GrantedAuthority> authorities = user
+                .getRoles()
+                .stream()
+                .map((role) -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+}
