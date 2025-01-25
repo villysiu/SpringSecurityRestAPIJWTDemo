248f17006a06cc4e7944808e8c37676b38c5c4dc
Villy Siu <villysiu@gmail.com>
2025-01-25 07:12:20 -0800

    added annotation

--- src/main/java/com/villysiu/springsecurityrestapi/config/SecurityConfig.java
+++ src/main/java/com/villysiu/springsecurityrestapi/config/SecurityConfig.java
@@ -1,48 +1,57 @@
 package com.villysiu.springsecurityrestapi.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-    private UserDetailsService userDetailsService;
+//    private UserDetailsService userDetailsService;
+//
+//
+//    public SecurityConfig(UserDetailsService userDetailsService){
+//        this.userDetailsService = userDetailsService;
+//    }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-    public SecurityConfig(UserDetailsService userDetailsService){
-        this.userDetailsService = userDetailsService;
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((auth) ->
+                        //authorize.anyRequest().authenticated()
+                        auth.requestMatchers(HttpMethod.GET, "/api/**", "index").permitAll()
+                                .requestMatchers(HttpMethod.POST,"/api/auth/**").permitAll()
+                                .anyRequest().authenticated()
+
+                );
+
+        return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-            .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests((auth) ->
-                //authorize.anyRequest().authenticated()
-                auth.requestMatchers(HttpMethod.GET, "/api/**", "index").permitAll()
-                    .requestMatchers(HttpMethod.POST,"/api/auth/**").permitAll()
-                    .anyRequest().authenticated()
 
-            );
-
-        return http.build();
-    }
 }
