2e8c0a09d12d74e4c1049573c439c21c9581145a
Villy Siu <villysiu@gmail.com>
2025-01-24 19:27:56 -0800

    setup entry point

---
+++ src/main/java/com/villysiu/springsecurityrestapi/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.villysiu.springsecurityrestapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests((auth) ->
+                //authorize.anyRequest().authenticated()
+                auth.requestMatchers(HttpMethod.GET, "/api/**", "index").permitAll()
+                    .requestMatchers(HttpMethod.POST,"/api/auth/**").permitAll()
+                    .anyRequest().authenticated()
+
+            );
+
+        return http.build();
+    }
+}
